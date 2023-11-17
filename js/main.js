78162b9f259cc2acd79f370799800a04a83cdb04
zeeeunee <wldmstv@gmail.com>
2023-11-17 13:14:46 +0900

    유튜브 데이터 호출 및 동적 DOM생성

--- js/main.js
+++ js/main.js
@@ -1,7 +1,27 @@
 const api_key = 'AIzaSyBQ0OBVJR5LwVP7O1wFRSbfMbLCLvWRLnE';
 const playList_id = 'PLM7Wu-2kzIQNKIs1hkRrlcXbwun3W_hWg';
 const base_url = 'https://www.googleapis.com/youtube/v3/playlistItems';
-const result_url = `${base_url}?part=snippet&key=${api_key}&playListId=${playList_id}&maxResults=5`;
+const num = 5;
+const result_url = `${base_url}?part=snippet&key=${api_key}&playlistId=${playList_id}&maxResults=${num}`;
+
+fetch(result_url)
+	.then((data) => data.json())
+	.then((json) => {
+		console.log(json.items);
+		let tags = '';
+		json.items.forEach((el) => {
+			tags += `
+        <article>
+          <img src=${el.snippet.thumbnails.standard.url} />
+          <h2>${el.snippet.title}</h2>
+          <p>${el.snippet.description}</p>
+          <span>${el.snippet.publishedAt}</span>
+        </article>
+      `;
+		});
+
+		document.body.innerHTML = tags;
+	});
 
 /*
 브라우저 url을 통해서 데이터 요청방식 : GET방식
