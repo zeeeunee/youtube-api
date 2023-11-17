678dbde5304775b3cbfad09ec3eb94fea772a8f1
zeeeunee <wldmstv@gmail.com>
2023-11-17 13:02:47 +0900

    수정

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const api_key = 'AIzaSyBQ0OBVJR5LwVP7O1wFRSbfMbLCLvWRLnE';
 const playList_id = 'PLM7Wu-2kzIQNKIs1hkRrlcXbwun3W_hWg';
 const base_url = 'https://www.googleapis.com/youtube/v3/playlistItems';
-const result_url = `${base_url}?part=snippet&api_key=${api_key}&playListId=${playList_id}&maxResults=5`;
+const result_url = `${base_url}?part=snippet&key=${api_key}&playListId=${playList_id}&maxResults=5`;
 
 /*
 브라우저 url을 통해서 데이터 요청방식 : GET방식
